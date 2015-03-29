683f20a16213f3c1fa8a6f09d3f52f5b28eb71b1
wang zuo <wzuoadjusted@gmail.com>
2015-03-29 18:40:02 +0800

    props update fix

--- lib/input-color.jsx
+++ lib/input-color.jsx
@@ -8,48 +8,85 @@ var rgbaColor = require('./rgba-color');
 var rgb2hsv = require('./rgb2hsv');
 var rgb2hex = require('./rgb2hex');
 
+var KEY_ENTER = 13;
+
+function getColor(cssColor) {
+  var rgba = colorParser(cssColor);
+  var r = rgba.r, g = rgba.g, b = rgba.b, a = rgba.a*100;
+  var hsv = rgb2hsv(r, g, b);
+
+  return objectAssign(hsv, {
+    r: r,
+    g: g,
+    b: b,
+    a: a,
+    hex: rgb2hex(r, g, b)
+  });
+}
+
 module.exports = React.createClass({
   displayName: 'InputColor',
 
-  getInitialState: function () {
+  getInitialState: function() {
     var cssColor = this.props.color;
-    var rgba = colorParser(cssColor);
-    var r = rgba.r, g = rgba.g, b = rgba.b, a = rgba.a*100;
-    var hsv = rgb2hsv(r, g, b);
-    var color = objectAssign(hsv, {
-      r: r,
-      g: g,
-      b: b,
-      a: a,
-      hex: rgb2hex(r, g, b)
-    });
 
     return {
-      color: color,
-      colorPicker: true
+      color: getColor(this.props.color),
+      colorPicker: true,
+      cssColor: cssColor
     };
   },
 
-  render: function () {
+  render: function() {
     var color = this.state.color;
     var r = color.r, g = color.g, b = color.b, a = color.a;
     var rgbaBackground = rgbaColor(r, g, b, a);
 
     return (
       <div className="m-input-color">
-        {this.state.colorPicker ? <ColorPicker color={color} onChange={this._onChange}/> : null}
+        {this.state.colorPicker ? <ColorPicker color={this.state.color} onChange={this._onChange}/> : null}
         <div className="css-input-box">
           <div className="css-color"
             style={{background:rgbaBackground}}
             onClick={this._onClick}></div>
-          <input className="css-input" type="text" value={'#'+color.hex}/>
+          <input className="css-input" type="text" value={this.state.cssColor}
+            onChange={this._onInputChange} onKeyUp={this._onInputKeyUp}/>
         </div>
       </div>
     );
   },
 
+  componentWillReceiveProps: function(nextProps) {
+    var cssColor = nextProps.color;
+    this.setState({
+      cssColor: cssColor,
+      color: getColor(cssColor)
+    });
+  },
+
+  change: function(cssColor) {
+    if(this.props.onChange) {
+      this.props.onChange(cssColor);
+    }
+  },
+
+  _onInputChange: function(e) {
+    this.setState({
+      cssColor: e.target.value
+    });
+  },
+
+  _onInputKeyUp: function(e) {
+    if(e.keyCode === KEY_ENTER) {
+      this.change(this.state.cssColor);
+    }
+  },
+
   _onChange: function(color) {
-    this.setState({color: color});
+    this.setState({
+      cssColor: '#'+color.hex,
+      color: color
+    });
   },
 
   _onClick: function() {
