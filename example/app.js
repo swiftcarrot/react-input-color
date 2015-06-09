3e70dde4e70e05f3ca1ea8c9999d0457cfaa6bc3
wang zuo <wzuoadjusted@gmail.com>
2015-06-09 19:13:11 +0800

    remove color support & defaultValue props

--- example/app.js
+++ example/app.js
@@ -1,6 +1,8 @@
+require('../lib/input-color.less');
+require('./app.less');
+
 var React = require('react');
 var InputColor = require('../lib/input-color.js');
-require('!style!css!less!../lib/input-color.less');
 
 var App = React.createClass({
   getInitialState: function() {
@@ -12,11 +14,19 @@ var App = React.createClass({
   render: function() {
     return (
       <div>
+        <div className="target" style={{width:100,height:100,background:this.state.color}}></div>
         <span>{'color value: ' + this.state.color}</span>
         <br/><br/>
-        <input type="color" value={this.state.color} onChange={this._onColorChange}/>
+        <input
+          type="color"
+          value={this.state.color}
+          defaultValue="#345678"
+          onChange={this._onColorChange}/>
         <br/><br/>
-        <InputColor value={this.state.color} onChange={this._onChange}/>
+        <InputColor
+          value={this.state.color}
+          defaultValue="#345678"
+          onChange={this._onChange}/>
       </div>
     );
   },
