4c5758658784c63bdfe6ae265a48b812d2fdb4e6
wang zuo <wzuoadjusted@gmail.com>
2015-08-03 21:15:56 +0800

    create gh-pages

--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
 all:
 	babel lib --out-dir dist
 	lessc lib/input-color.less > dist/input-color.css
+	webpack -p
 clean:
 	rm dist/*
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   entry: './example/app.js',
   output: {
-    path: __dirname + '/builds',
-    filename: 'app.js',
-    publicPath: "/builds/",
+    path: __dirname + '/example',
+    filename: 'bundle.js',
+    publicPath: "/example/",
   },
   module: {
     loaders: [
