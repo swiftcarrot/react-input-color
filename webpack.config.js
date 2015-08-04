203d333e3ea91f8f82f157620699c28fce598266
wang zuo <wzuoadjusted@gmail.com>
2015-08-04 22:48:15 +0800

    color slider styling fix

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,7 @@ module.exports = {
   module: {
     loaders: [
       {test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'},
+      {test: /\.css$/, loader: "style-loader!css-loader"},
       {test: /\.less$/, loader: "style-loader!css-loader!less-loader"}
     ]
   },
